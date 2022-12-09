cd620cd72a05f314af7fad2c42d156fa8e43fc9a
gkirtirajprdxn <kirtirajg@axioned.com>
2022-12-09 17:44:15 +0530

    video container added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import video from './dummy_video.mp4';
+import video1 from './dummy_video1.mp4';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello World!</h1>
+      <div className="card-container">
+        <video controls loop muted autoPlay>
+          <source src={video} />Your browser does not support the video tag.
+        </video>
+      </div>
+
+      <div className="card-container-1">
+        <video controls loop muted autoPlay>
+          <source src={video1} />Your browser does not support the video tag.
+        </video>
+      </div>
     </div>
   );
 }
